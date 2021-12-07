0c3a0bbd1fbefcb1a6ba738eea0737f7e1838774
spiritsliu <spiritsliu@tencent.com>
2021-12-07 17:45:18 +0800

    玩转vscode: 快捷键

---
+++ online-course/玩转vscode/5 快捷键进阶.js
@@ -0,0 +1,21 @@
+/*
+1、删除当行 cmd+shift+k
+2、剪切当行 cmd+x
+3、在当前行之前开始新的一行 cmd+shift+enter
+4、在当前行之后开始新的一行 cmd+enter
+5、移动代码 option+上下 
+6、复制移动代码 option+shift+上下 如果代码块发生改变 会自动调整位置
+7、注释 cmd+/
+8、代码格式化 shift+option+f
+9、格式化缩进 重新缩进选中行（命令行搜索缩进）
+10、调换光标前后字符位置 control + t
+11、大小写转化 命令行搜索 转换为大（小）写
+12、合并代码行 control+j
+13、行排序按首字母重新排序  命令行搜索 生（降）序
+14、撤销光标选中或移动 cmd+u
+*/
+
+function lqq () {
+    console.log(`love qq 1`)
+    console.log(`love qq 2`)
+}
---
+++ online-course/玩转vscode/6 DRY 多光标特性.js
@@ -0,0 +1,10 @@
+/*
+1、创建多光标 option+鼠标点击 或者cmd+option+上下
+2、cmd+d 第一次按会寻找附近单词 第二次会找到相同单词 创建光标
+3、shift+option+i 在选中代码每行末尾加入光标
+*/
+
+function lqq () {
+    console.log(`love qq 2`)
+    console.log(`love qq 1`)
+}
