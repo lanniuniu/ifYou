49e55f11854f379dc162a5691b4121aca867d6af
lanniuniu <1129638103@qq.com>
2022-08-17 15:45:13 +0800

    玩转VSCode: 代码折叠、搜索

---
+++ online-course/玩转vscode/11 代码折叠、小地图、面包屑.js
@@ -0,0 +1,20 @@
+/*
+代码折叠与展开
+
+Cmd + Option + 左（右）方括号 当前光标所处的最内层的、可以被折叠的代码就会被折叠起来。
+
+全部展开 Cmd+K Cmd+J
+全部折叠 Cmd+K Cmd+0
+*/
+
+function lqq() {
+  while (1) {
+    console.log(`love qq 1`);
+    console.log(`love qq 2`);
+  }
+}
+
+/*
+面包屑
+提供新的文件跳转 文件查看的方式
+*/
