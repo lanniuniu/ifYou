443483fe1984768e29e036da8a3048450c8e3176
lanniuniu <1129638103@qq.com>
2022-09-14 15:09:39 +0800

    玩转vscode: workflow

--- online-course/玩转vscode/17 终端.js
+++ online-course/玩转vscode/17 终端.js
@@ -4,3 +4,9 @@ control + ~ 打开/隐藏 终端窗口
 control+shift+ ~ 打开新的窗口
 command + \ 拆分窗口
 */
+
+/*
+control + A 把光标移到开头
+option + 左右 光标单词移动
+cmd + f 出搜索框
+*/
---
+++ online-course/玩转vscode/18 workflow.js
@@ -0,0 +1,28 @@
+/*
+执行任务：自动检测任务、自定义任务
+
+搜索"配置任务"，可以看到默认的任务和最下面的任务模板
+
+搜索”运行任务“，可以运行所配置的任务
+*/
+
+/*
+Command相关属性和特殊处理
+参数 args
+
+{
+ "label": "echo",
+ "type": "shell",
+ "command": "echo",
+ "args": [
+   {
+     "value": "Hello World",
+     "quoting": "escape"
+   }
+ ]
+}
+
+
+结果分析
+problemMatcher
+*/
