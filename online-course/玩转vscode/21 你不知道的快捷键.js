f97eec23b2c8eb137acf3eff64f94a78727a4088
lanniuniu <1129638103@qq.com>
2022-12-22 14:02:51 +0800

    fix: 循环的结束方式

---
+++ online-course/玩转vscode/21 你不知道的快捷键.js
@@ -0,0 +1,9 @@
+/*
+切换横向（纵向）编辑器组  Cmd + Option + 0或者命令
+
+
+tab 之间跳转  Cmd + Option + 左右
+
+Grid 网格布局
+
+*/
--- something to improve/关于js的tips.js
+++ something to improve/关于js的tips.js
@@ -9,3 +9,44 @@ cb&&cb()
 cb&&cb()可以理解为，cb这个回调函数存在时才执行cb回调函数。
  */
 
+/**
+ * 2.循环的结束方式
+let arrs=[1,2,3,4,5]
+ 
+//1: forEach循环
+try{
+    arrs.forEach(function(i){
+        if(i == 2){
+            // return; // 退出当前循环
+            throw 'jump out'//退出整个循环
+        }
+        console.log(i)
+    })
+}catch(e){
+ 
+}
+ 
+//2: for循环
+for(var i=0, len = arrs.length ; i< len ; i++){
+    if(i == 2){
+       //  continue;// 退出当前循环
+        break;//退出整个循环
+    }
+    console.log(arrs[i]);   
+}
+ 
+//3 for-of循环
+for(var i of arrs){
+    if(i == 2){
+       //  continue;// 退出当前循环
+        break;//退出整个循环
+   }
+    console.log(i);   
+}
+ 
+//4 for-in循环
+ 
+退出方法同for循环。
+ * 
+ */
+
