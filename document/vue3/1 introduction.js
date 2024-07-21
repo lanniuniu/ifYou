c099a224856ccefb2e140cd9d809919035a13db2
lanniuniu <1129638103@qq.com>
2024-07-21 22:00:01 +0800

    vue3: tow api style

---
+++ document/vue3/1 introduction.js
@@ -0,0 +1,7 @@
+/**
+ * option api and composition api
+ * they are different interfaces powered by the exact some underlying system
+ * the option api is implemented on the top of Composition API
+ * 
+ * Composition API is more powerful for organizing and reusing logic
+ */
