ee504922615f8701fd756b16d56da06f1d8bedae
lanniuniu <1129638103@qq.com>
2024-07-28 21:45:16 +0800

    vue3: app create

---
+++ document/vue3/2 create vue3 app.js
@@ -0,0 +1,15 @@
+/**
+ * root component
+ * 1. create vue instance
+ * 2. create root app
+ * 3. mount root app
+ * 
+ * App Configurations
+ * app.config.errorHandler = (err) => {
+ *   console.log(err);
+ * }
+ * 
+ * app.component('TodoDeleteButton', TodoDeleteButton)
+ * 
+ * multiple app instances
+ */
